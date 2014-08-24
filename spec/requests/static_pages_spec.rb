468fd5d87ed6bb11c10bf089c769b42ca5bda938
Princess Katie <kdschilling@gmail.com>
2014-08-24 17:46:13 -0500

    added title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  let(:base_title) {"Teach the foxes to tweet | "}
+  let(:base_title) {"Teach the sleepy foxes to tweet"}
 
   describe "Home page" do
     it "should have the content 'demo app'" do
@@ -11,9 +11,9 @@ describe "Static Pages" do
       expect(page).to have_content('Demo App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}Home")
+      expect(page).to have_title("#{base_title}")
     end
   end
   
@@ -26,7 +26,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title}Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -38,7 +38,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title}About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 end
